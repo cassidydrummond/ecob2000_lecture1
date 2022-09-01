193c3f0fd152bbfa2f33c50cfd54ac3ca1b37a21
cassidydrummond <112599304+cassidydrummond@users.noreply.github.com>
2022-08-31 23:04:32 -0400

    Add files via upload

---
+++ ecob2000_lecture1.R
@@ -0,0 +1,4 @@
+#I think it's interesting that the average age (both for males and females) of respondents was in their mid 50s. With increased skeptism surrounding COVID and government agencies, I'm surprised the average age was of an older generation.
+#I found the difference between those vaccinated and those not to be interestingly large; As the gap within US is about 15% (and the data shows 11.5%). Although, I am curious when this dataset was created and who conducted the survey. I think those factors would impact the audience participating as well as acknowledge accessibility issues.
+
+#1 Adeel Arshid and Ksenia Guseinova
